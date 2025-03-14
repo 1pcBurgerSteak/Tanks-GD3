4fc913ceef7bce9bc8dd45ed67e3607b19071ae0
Giocumber <giosalceda@gmail.com>
2025-03-14 18:20:32 +0800

    Added Crate Manager and Crate Power Ups

--- Assets/Script/PlayerScripts/PlayerController.cs
+++ Assets/Script/PlayerScripts/PlayerController.cs
@@ -4,60 +4,58 @@ using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Complete
-{
-    public class PlayerController : MonoBehaviour
-    {
-        public string controlType;
-        private GameObject playerManager;
-        private PlayerMovement playerMovement;
-        private PlayerShooting playerShooting;
-        private GameObject playerObject;
-        public GameObject playerPrefab;
-        void Start()
-        {
-            playerManager = GameObject.Find("PlayerManager");
-            if (playerManager != null)
-            {
-                transform.SetParent(playerManager.transform);
-            }
-            else
-            {
-                Debug.LogWarning("PlayerManager is not assigned in PlayerController.");
-            }
-            playerObject = Instantiate(playerPrefab, transform.position,Quaternion.identity);
-        }
-        void Update()
-        {
-            if(playerObject != null)
-            {
-                playerMovement = playerObject.GetComponent<PlayerMovement>();
-                playerShooting = playerObject.GetComponent<PlayerShooting>();
-            }
-        }
 
-        public void OnMove(InputAction.CallbackContext context)
+public class PlayerController : MonoBehaviour
+{
+    public string controlType;
+    private GameObject playerManager;
+    private PlayerMovement playerMovement;
+    private PlayerShooting playerShooting;
+    private GameObject playerObject;
+    public GameObject playerPrefab;
+    void Start()
+    {
+        playerManager = GameObject.Find("PlayerManager");
+        if (playerManager != null)
         {
-            Vector2 inputValue = context.ReadValue<Vector2>();
-            Debug.Log($"{controlType}{inputValue}");
-            if(playerMovement != null)
-            {
-                playerMovement.UpdateMovement(inputValue);
-            }
+            transform.SetParent(playerManager.transform);
         }
-        public void OnShoot(InputAction.CallbackContext context) 
+        else
         {
-            Debug.Log($"{controlType}: Shoot");
-            if(playerShooting != null)
+            Debug.LogWarning("PlayerManager is not assigned in PlayerController.");
+        }
+        playerObject = Instantiate(playerPrefab, transform.position, Quaternion.identity);
+    }
+    void Update()
+    {
+        if (playerObject != null)
+        {
+            playerMovement = playerObject.GetComponent<PlayerMovement>();
+            playerShooting = playerObject.GetComponent<PlayerShooting>();
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 inputValue = context.ReadValue<Vector2>();
+        Debug.Log($"{controlType}{inputValue}");
+        if (playerMovement != null)
+        {
+            playerMovement.UpdateMovement(inputValue);
+        }
+    }
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        Debug.Log($"{controlType}: Shoot");
+        if (playerShooting != null)
+        {
+            if (context.performed)
             {
-                if(context.performed)
-                {
-                    playerShooting.Shoot();
-                }
-                if(context.canceled)
-                {
-                    playerShooting.UnShoot();
-                }
+                playerShooting.Shoot();
+            }
+            if (context.canceled)
+            {
+                playerShooting.UnShoot();
             }
         }
     }
--- Assets/Script/PlayerScripts/PlayerShooting.cs
+++ Assets/Script/PlayerScripts/PlayerShooting.cs
@@ -1,88 +1,109 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Complete
-{   
-    public class PlayerShooting : MonoBehaviour
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;              // Used to identify the different players.
+    public Rigidbody m_Shell;                   // Prefab of the shell.
+    public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+    public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
+    public float m_MinLaunchForce = 15f;        // The force given to the shell if the fire button is not held.
+    public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
+    public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
+
+
+    private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
+    private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
+    private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+
+    public bool canMultiShoot = false;
+
+    private void OnEnable()
     {
-        public int m_PlayerNumber = 1;              // Used to identify the different players.
-        public Rigidbody m_Shell;                   // Prefab of the shell.
-        public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
-        public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
-        public float m_MinLaunchForce = 15f;        // The force given to the shell if the fire button is not held.
-        public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
-        public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
+        m_CurrentLaunchForce = m_MinLaunchForce;
+        m_AimSlider.value = m_MinLaunchForce;
+    }
 
 
-        private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
-        private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
-        private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+    private void Start()
+    {
+
+        // The rate that the launch force charges up is the range of possible forces by the max charge time.
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+        m_Fired = true;
+    }
 
 
-        private void OnEnable()
+    private void Update()
+    {
+        // The slider should have a default value of the minimum launch force.
+        m_AimSlider.value = m_MinLaunchForce;
+
+        // If the max force has been exceeded and the shell hasn't yet been launched...
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
         {
-            m_CurrentLaunchForce = m_MinLaunchForce;
-            m_AimSlider.value = m_MinLaunchForce;
-        }
+            // ... use the max force and launch the shell.
+            m_CurrentLaunchForce = m_MaxLaunchForce;
 
-
-        private void Start ()
-        {
-
-            // The rate that the launch force charges up is the range of possible forces by the max charge time.
-            m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
-            m_Fired = true;
-        }
-
-
-        private void Update ()
-        {
-            // The slider should have a default value of the minimum launch force.
-            m_AimSlider.value = m_MinLaunchForce;
-
-            // If the max force has been exceeded and the shell hasn't yet been launched...
-            if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
-            {
-                // ... use the max force and launch the shell.
-                m_CurrentLaunchForce = m_MaxLaunchForce;
-                Fire ();
-            }
-            // Otherwise, if the fire button is being held and the shell hasn't been launhed yet...
-            else if (!m_Fired)
-            {
-                m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
-
-                m_AimSlider.value = m_CurrentLaunchForce;
-            } 
-        }
-        public void Shoot()
-        {
-            m_Fired = false;
-            m_CurrentLaunchForce = m_MinLaunchForce;
-        }
-        public void UnShoot()
-        {
-            if (!m_Fired)
-            {
+            if (canMultiShoot)
+                StartCoroutine(MultiBulletShoot());
+            else
                 Fire();
-            }
         }
-
-
-        private void Fire ()
+        // Otherwise, if the fire button is being held and the shell hasn't been launhed yet...
+        else if (!m_Fired)
         {
-            // Set the fired flag so only Fire is only called once.
-            m_Fired = true;
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
 
-            // Create an instance of the shell and store a reference to it's rigidbody.
-            Rigidbody shellInstance =
-                Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+            m_AimSlider.value = m_CurrentLaunchForce;
+        }
+    }
+    public void Shoot()
+    {
+        m_Fired = false;
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+    public void UnShoot()
+    {
+        if (!m_Fired)
+        {
+            if (canMultiShoot)
+            {
+                StartCoroutine(MultiBulletShoot());
+                return;
+            }
 
-            // Set the shell's velocity to the launch force in the fire position's forward direction.
-            shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; 
+            Fire();
+        }
+    }
 
-            // Reset the launch force.  This is a precaution in case of missing button events.
-            m_CurrentLaunchForce = m_MinLaunchForce;
+
+    private void Fire()
+    {
+        // Set the fired flag so only Fire is only called once.
+        m_Fired = true;
+
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody shellInstance =
+            Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+        // Reset the launch force.  This is a precaution in case of missing button events.
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+
+    public IEnumerator MultiBulletShoot()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+            yield return new WaitForSeconds(0.2f);
         }
     }
 }
