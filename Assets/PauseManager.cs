b23aa6db68877c5c991eab17674ac2488ca37d3e
Tt0oNnYy <denzelguinoo1104@gmail.com>
2025-03-18 14:40:38 +0800

    PauseMenuV2
    pause menu placeholder

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public TextMeshProUGUI countdownText;
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        countdownText.gameObject.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false); // Close the pause menu immediately
+        StartCoroutine(ResumeCountdown());
+    }
+
+    IEnumerator ResumeCountdown()
+    {
+        countdownText.gameObject.SetActive(true);
+
+        for (int i = 3; i > 0; i--)
+        {
+            countdownText.text = i.ToString();
+            yield return new WaitForSecondsRealtime(1f);
+        }
+
+        countdownText.gameObject.SetActive(false); // Hide countdown text
+        Time.timeScale = 1f;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadHome()
+    {
+        Time.timeScale = 1f; // Reset time before loading home
+        SceneManager.LoadScene("Menu");
+    }
+}
